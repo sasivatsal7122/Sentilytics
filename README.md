4ffacc83feb77f60fa604c1c2203dfff48fb17bd
sasivatsal7122 <sasivatsal7122@gmail.com>
2023-06-06 00:51:33 +0530

    fixed 2 major bugs in sentiment analysis

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# sentiview-vidalytics
+# API Documentation
+
+This documentation provides an overview of the available endpoints and their usage in the API.
+
+## Base URL
+
+The base URL for all API endpoints is `http://your-api-domain.com`.
+
+## Endpoints
+
+### 1. Root
+
+- **Endpoint**: `/`
+- **Method**: GET
+- **Description**: Returns a simple "Hello World" message.
+- **Example**:
+    ```
+    GET / HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+### 2. Scrape Channel
+
+- **Endpoint**: `/scrape_channel/`
+- **Method**: GET
+- **Description**: Scrapes channel information based on the provided user ID and channel username.
+- **Parameters**:
+    - `userID` (required): User ID
+    - `channelUsername` (required): Channel Username
+- **Example**:
+    ```
+    GET /scrape_channel/?userID=12345&channelUsername=mychannel HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+### 3. Get Latest 20
+
+- **Endpoint**: `/get_latest20/`
+- **Method**: GET
+- **Description**: Retrieves the latest 20 records.
+- **Parameters**:
+    - `channelID` (required): Channel ID
+- **Example**:
+    ```
+    GET /get_latest20/?channelID=abcde HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+### 4. Get High-Level Comments
+
+- **Endpoint**: `/get_hlcomments/`
+- **Method**: GET
+- **Description**: Retrieves high-level comments for a specific video.
+- **Parameters**:
+    - `videoID` (required): Video ID
+- **Example**:
+    ```
+    GET /get_hlcomments/?videoID=12345 HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+### 5. Get Low-Level Comments
+
+- **Endpoint**: `/get_llcomments/`
+- **Method**: GET
+- **Description**: Retrieves low-level comments for a specific video.
+- **Parameters**:
+    - `videoID` (required): Video ID
+- **Example**:
+    ```
+    GET /get_llcomments/?videoID=12345 HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+### 6. Perform Sentilytics
+
+- **Endpoint**: `/perform_sentilytics/`
+- **Method**: GET
+- **Description**: Performs sentiment analysis on comments for a specific video.
+- **Parameters**:
+    - `videoID` (required): Video ID
+- **Example**:
+    ```
+    GET /perform_sentilytics/?videoID=12345 HTTP/1.1
+    Host: your-api-domain.com
+    ```
+
+Please note that the above examples are for illustrative purposes, and you should replace the values with actual data when making API requests.
+
+
--- main.py
+++ main.py
@@ -1,37 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Query
+from fastapi.responses import JSONResponse
 
 from process import get_channel_info,get_latest20,get_HighLvlcomments,get_Lowlvlcomments
 
 from sentimentAnalysis import performSentilytics
 
-app = FastAPI()
 
-@app.get("/")
+app = FastAPI()
+# Create an APIRouter instance for grouping related routes
+router = APIRouter()
+
+# Define the root route
+@router.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/scrape_channel/")
-async def root(userID: str, channelUsername: str):
-    
-    return get_channel_info(userID, channelUsername)
+# Define the "scrape_channel" route
+@router.get("/scrape_channel/")
+async def scrape_channel(userID: str = Query(..., description="User ID"),
+                         channelUsername: str = Query(..., description="Channel Username")):
+    """
+    Endpoint to scrape channel information.
+    """
+    channel_info = get_channel_info(userID, channelUsername)
+    return JSONResponse(content=channel_info)
 
-@app.get("/get_latest20/")
-async def root(channelID: str):
-    
-    return get_latest20(channelID)
+# Define the "get_latest20" route
+@router.get("/get_latest20/")
+async def get_latest20(channelID: str = Query(..., description="Channel ID")):
+    """
+    Endpoint to get the latest 20 records.
+    """
+    latest_records = get_latest20(channelID)
+    return JSONResponse(content=latest_records)
 
-@app.get("/get_hlcomments/")
-async def root(videoID: str):
-    
-    return get_HighLvlcomments(videoID)
+# Define the "get_hlcomments" route
+@router.get("/get_hlcomments/")
+async def get_hlcomments(videoID: str = Query(..., description="Video ID")):
+    """
+    Endpoint to get high-level comments.
+    """
+    hl_comments = get_HighLvlcomments(videoID)
+    return JSONResponse(content=hl_comments)
 
-@app.get("/get_llcomments/")
-async def root(videoID: str):
-    
-    return get_Lowlvlcomments(videoID)
+# Define the "get_llcomments" route
+@router.get("/get_llcomments/")
+async def get_llcomments(videoID: str = Query(..., description="Video ID")):
+    """
+    Endpoint to get low-level comments.
+    """
+    ll_comments = get_Lowlvlcomments(videoID)
+    return JSONResponse(content=ll_comments)
 
-@app.get("/perform_sentilytics/")
-async def root(videoID: str):
-    
-    return performSentilytics(videoID)
+# Define the "perform_sentilytics" route
+@router.get("/perform_sentilytics/")
+async def perform_sentilytics(videoID: str = Query(..., description="Video ID")):
+    """
+    Endpoint to perform sentiment analysis on comments.
+    """
+    sentilytics_result = performSentilytics(videoID)
+    return JSONResponse(content=sentilytics_result)
 
+# Include the router in the main app
+app.include_router(router)
