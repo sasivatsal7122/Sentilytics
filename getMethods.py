74b80976a7584c1c6388848d2166d90b224d1187
sasivatsal7122 <sasivatsal7122@gmail.com>
2023-08-06 12:17:53 +0530

    eliminated few unwanted functions

--- getMethods.py
+++ getMethods.py
@@ -1,12 +1,21 @@
-import sqlite3
+import pymysql
 import json 
 
-conn = sqlite3.connect('sentilytics.db')
-cursor = conn.cursor()
+
+conn_params = {
+    "host": "127.0.0.1",
+    "port": 3306,                # Change to your MySQL port if necessary
+    "user": "admin",
+    "password": "admin",
+    "db": "sentilytics",
+}
+
+connection = pymysql.connect(**conn_params)
+cursor = connection.cursor()
 
 def get_channel_info(channel_id):
     
-    query = f"SELECT * FROM Channels WHERE channel_id = ?;"
+    query = f"SELECT * FROM Channels WHERE channel_id = %s;"
     cursor.execute(query, (channel_id,))
     channel_data = cursor.fetchall()
     
@@ -17,7 +26,7 @@ def get_channel_info(channel_id):
         channel_info['channel_logo_url'] = json.loads(channel_info['channel_logo_url'])
         formatted_data = {channel_id: channel_info}
     
-    query = f"SELECT * FROM Videos WHERE channel_id = ?;"
+    query = f"SELECT * FROM Videos WHERE channel_id = %s;"
     cursor.execute(query, (channel_id,))
     videos_data = cursor.fetchall()
     videos = []
@@ -30,9 +39,10 @@ def get_channel_info(channel_id):
                       "video": videos}
     return formatted_data
 
+
 def get_monthly_stats(channel_id):
     
-    query = f"SELECT * FROM MonthlyStats WHERE channel_id = ?;"
+    query = f"SELECT * FROM MonthlyStats WHERE channel_id = %s;"
     cursor.execute(query, (channel_id,))
     stats_data = cursor.fetchall()
 
@@ -46,7 +56,7 @@ def get_monthly_stats(channel_id):
 
 def get_video_stats(channel_id):
 
-    query = f"SELECT * FROM VideoStats WHERE channel_id = ?;"
+    query = f"SELECT * FROM VideoStats WHERE channel_id = %s;"
     cursor.execute(query, (channel_id,))
     stats_data = cursor.fetchall()
 
@@ -69,7 +79,7 @@ def get_emoji_analysis(channel_id):
         SELECT v.vid_id, v.vid_title, e.highlvl_freq
         FROM Videos v
         INNER JOIN Emoji_Frequency e ON v.vid_id = e.vid_id
-        WHERE v.channel_id = ?
+        WHERE v.channel_id = %s
     ''', (channel_id,))
     results = cursor.fetchall()
 
@@ -86,171 +96,74 @@ def get_emoji_analysis(channel_id):
 
     return data
 
-def get_video_comments(video_id):
+def select_data_by_scan_id(scan_id):
     
-    query = '''
-        SELECT C.comment_id, C.comment, SA.sentiment
-        FROM Comments_SentimentAnalysis SA
-        LEFT JOIN Comments_Unfiltered C ON SA.vid_id = C.vid_id AND SA.comment_id = C.comment_id
-        WHERE SA.vid_id = ?
-    '''
-
-    cursor.execute(query, (video_id,))
-    rows = cursor.fetchall()
-
-    video_comments = {}
-
-    for row in rows:
-        comment_id = row[0]
-        comment = row[1]
-        sentiment = row[2]
-
-        video_comments[comment_id] = {
-            'Comment': comment,
-            'Sentiment': sentiment
-        }
-
-    import json
-    with open(f'{video_id}_comments.json', 'w') as json_file:
-        json.dump({video_id: video_comments}, json_file, indent=4)
-
-    return {video_id: video_comments}
-
-
-def select_data_by_user_id(user_id):
+    data_dict = {
+        'channel': '',
+        'video_stats':'',
+        'monthly_stats':'',
+        'scan_info':'',
+        'videos':'',
+        
+    }
     
-    def get_channel_id_by_user_id(cursor, user_id):
-        cursor.execute("SELECT channel_id FROM Channels WHERE user_id=?", (user_id,))
+    def get_channel_id_by_scan_id(cursor, scan_id):
+        cursor.execute("SELECT channel_id FROM Channels WHERE scan_id=%s", (scan_id,))
         result = cursor.fetchone()
         return result[0] if result else None
 
     def get_video_ids_by_channel_id(cursor, channel_id):
-        cursor.execute("SELECT vid_id FROM Videos WHERE channel_id=?", (channel_id,))
+        cursor.execute("SELECT vid_id FROM Videos WHERE channel_id=%s", (channel_id,))
         result = cursor.fetchall()
         return [row[0] for row in result]
     
-    channel_id = get_channel_id_by_user_id(cursor, user_id)
+    channel_id = get_channel_id_by_scan_id(cursor, scan_id)
 
     if not channel_id:
         return None
 
-    data_dict = {}
-
-    cursor.execute("SELECT * FROM Channels WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM Channels WHERE channel_id=%s", (channel_id,))
     channel_columns = [column[0] for column in cursor.description]
 
-    cursor.execute("SELECT * FROM Channels WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM Channels WHERE channel_id=%s", (channel_id,))
     channel_data = cursor.fetchone()
-    data_dict['Channels'] = dict(zip(channel_columns, channel_data))
+    data_dict['channel'] = dict(zip(channel_columns, channel_data))
 
     video_ids = get_video_ids_by_channel_id(cursor, channel_id)
 
-    cursor.execute("SELECT * FROM Videos WHERE vid_id IN ({})".format(','.join(['?'] * len(video_ids))), video_ids)
+    cursor.execute("SELECT * FROM Videos WHERE vid_id IN ({})".format(','.join(['%s'] * len(video_ids))), video_ids)
     video_columns = [column[0] for column in cursor.description]
 
-    cursor.execute("SELECT * FROM Videos WHERE vid_id IN ({})".format(','.join(['?'] * len(video_ids))), video_ids)
+    cursor.execute("SELECT * FROM Videos WHERE vid_id IN ({})".format(','.join(['%s'] * len(video_ids))), video_ids)
     video_data = cursor.fetchall()
     data_dict['Videos'] = [dict(zip(video_columns, row)) for row in video_data]
 
     for table_name in ['Emoji_Frequency', 'Video_Rankings', 'VideoStats']:
-        if table_name == 'VideoStats':
-            cursor.execute("SELECT * FROM {} WHERE video_id IN ({})".format(table_name, ','.join(['?'] * len(video_ids))), video_ids)
-            table_columns = [column[0] for column in cursor.description]
-            table_data = cursor.fetchall()
-            data_dict[table_name] = [dict(zip(table_columns, row)) for row in table_data]
-        else:
-            cursor.execute("SELECT * FROM {} WHERE vid_id IN ({})".format(table_name, ','.join(['?'] * len(video_ids))), video_ids)
-            table_columns = [column[0] for column in cursor.description]
-            table_data = cursor.fetchall()
-            data_dict[table_name] = [dict(zip(table_columns, row)) for row in table_data]
+        cursor.execute("SELECT * FROM {} WHERE vid_id IN ({})".format(table_name, ','.join(['%s'] * len(video_ids))), video_ids)
+        table_columns = [column[0] for column in cursor.description]
+        table_data = cursor.fetchall()
+        data_dict[table_name] = [dict(zip(table_columns, row)) for row in table_data]
 
     sentiment_data = []
     for vid_id in video_ids:
-        cursor.execute("SELECT * FROM Comments_SentimentAnalysis WHERE vid_id=?", (vid_id,))
+        cursor.execute("SELECT * FROM Comments_SentimentAnalysis WHERE vid_id=%s", (vid_id,))
         sentiment_data.extend(cursor.fetchall())
     sentiment_columns = [column[0] for column in cursor.description]
     data_dict['Comments_SentimentAnalysis'] = [dict(zip(sentiment_columns, row)) for row in sentiment_data]
 
-    cursor.execute("SELECT * FROM MonthlyStats WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM MonthlyStats WHERE channel_id=%s", (channel_id,))
     monthly_stats_columns = [column[0] for column in cursor.description]
 
-    cursor.execute("SELECT * FROM MonthlyStats WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM MonthlyStats WHERE channel_id=%s", (channel_id,))
     monthly_stats_data = cursor.fetchall()
     data_dict['MonthlyStats'] = [dict(zip(monthly_stats_columns, row)) for row in monthly_stats_data]
 
-    cursor.execute("SELECT * FROM ScanInfo WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM ScanInfo WHERE channel_id=%s", (channel_id,))
     scan_info_columns = [column[0] for column in cursor.description]
 
-    cursor.execute("SELECT * FROM ScanInfo WHERE channel_id=?", (channel_id,))
+    cursor.execute("SELECT * FROM ScanInfo WHERE channel_id=%s", (channel_id,))
     scan_info_data = cursor.fetchall()
     data_dict['ScanInfo'] = [dict(zip(scan_info_columns, row)) for row in scan_info_data]
-
-    with open('data.json', 'w') as json_file:
-        json.dump(data_dict, json_file, indent=4)
-    
+   
     return data_dict
 
-def insert_data_into_tables(data):
-    
-    conn = sqlite3.connect('sentilyticsReplica.db')
-    cursor = conn.cursor()
-
-    # Insert data into the Channels table
-    for row in data['Channels']:
-        cursor.execute('''INSERT INTO Channels (user_id, channel_id, channel_title, channel_description,
-                                                subscriber_count, total_videos_count, total_views_count,
-                                                partial_likes_count, partial_comments_count, partial_views_count,
-                                                channel_created_date, channel_logo_url)
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-                        row[8], row[9], row[10], row[11]))
-
-    # Insert data into the Videos table
-    for row in data['Videos']:
-        cursor.execute('''INSERT INTO Videos (channel_id, vid_id, vid_title, vid_view_cnt, vid_like_cnt,
-                                              vid_comment_cnt, vid_url, vid_desc, vid_duration,
-                                              vid_published_at, vid_thumbnail)
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-                        row[8], row[9], row[10]))
-
-    # Insert data into the Emoji_Frequency table
-    for row in data['Emoji_Frequency']:
-        cursor.execute('''INSERT INTO Emoji_Frequency (vid_id, highlvl_freq)
-                          VALUES (?, ?)''',
-                       (row[0], row[1]))
-
-    # Insert data into the Comments_SentimentAnalysis table
-    for row in data['Comments_SentimentAnalysis']:
-        cursor.execute('''INSERT INTO Comments_SentimentAnalysis (vid_id, comment_id, comment, sentiment)
-                          VALUES (?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3]))
-
-    # Insert data into the Video_Rankings table
-    for row in data['Video_Rankings']:
-        cursor.execute('''INSERT INTO Video_Rankings (vid_id, keyword, results_vidID, results_vidurl,
-                                                      results_vidTitle, results_vidDesc, results_vidDuration,
-                                                      results_vidViewcnt, results_vidDt)
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
-
-    # Insert data into the VideoStats table
-    for row in data['VideoStats']:
-        cursor.execute('''INSERT INTO VideoStats (channel_id, video_id, date, vid_title, vid_view_cnt,
-                                                  vid_like_cnt, vid_comment_cnt, category)
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
-
-    # Insert data into the MonthlyStats table
-    for row in data['MonthlyStats']:
-        cursor.execute('''INSERT INTO MonthlyStats (channel_id, date, channel_subs, overall_views)
-                          VALUES (?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3]))
-
-    # Insert data into the ScanInfo table
-    for row in data['ScanInfo']:
-        cursor.execute('''INSERT INTO ScanInfo (channel_id, phase, start_time, end_time, success, notes)
-                          VALUES (?, ?, ?, ?, ?, ?)''',
-                       (row[0], row[1], row[2], row[3], row[4], row[5]))
-
-    conn.commit()
