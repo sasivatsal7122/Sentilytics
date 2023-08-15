4eff5beea5867d2db3ff838c61a99b1e29149c82
sasivatsal7122 <sasivatsal7122@gmail.com>
2023-08-15 14:08:20 +0530

    added scanID col in scaninfo table

--- createReplicatables.py
+++ createReplicatables.py
@@ -123,6 +123,7 @@ def createReplicaTables():
     # Create the ScanInfo table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS ScanInfo (
+        scan_id VARCHAR(255),
         channel_id VARCHAR(255),
         phase VARCHAR(255),
         start_time VARCHAR(255),
